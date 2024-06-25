e2a3d570ed253cdc76c6ba4dda141d8fe79a3066
Andrielson Ferreira da Silva <andrielson@gmail.com>
2024-06-25 15:17:39 -0300

    chore: add IDX

---
+++ .idx/dev.nix
@@ -0,0 +1,26 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [ 
+        pkgs.graalvm-ce
+        pkgs.maven
+    ];
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ 
+        "vscjava.vscode-java-pack" 
+        "redhat.vscode-quarkus" 
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install = "./mvnw dependency:resolve";
+      };
+      # Runs when a workspace is (re)started
+      onStart = { run-server = "./devserver.sh"; };
+    };
+  };
+}
